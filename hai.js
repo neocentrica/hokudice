987ca821964ea7b8bbb829bf2ab2b3f46b7715c5
neocentrica <64868274+neocentrica@users.noreply.github.com>
2022-04-30 14:30:14 -0500

    Update hai.js

--- hai.js
+++ hai.js
@@ -1,24 +1,24 @@
 hom = ["reverbera en la arena",
-        "cejas con pelos en el medio",
+        "cejas con vellos en el medio",
         "en la cara se unta crema",
-        "de sus labios la piel descaspa",
-        "no se traga el eructo",
+        "de los labios la piel descaspa",
+        "no se tapa el eructo",
         "desde la sala lo escuchan",
         "arden las rodillas raspadas",
         "en el subsuelo se acuesta",
         "en la clase se cabecea",
         "por insomnio me acomodo"];
 
-nat = ["del oído se lo saca",
+nat = ["de la oreja lo saca",
         "recién nacidos polluelos",
         "el pantano se entiesa",
-        "se estremecen las ramas",
-        "la cloaca se rebosa",
+        "las ramas se estremecen",
+        "el contador se rebosa",
         "las hojas secas se quiebran",
         "las freidoras efervescen",
-        "rayando chilla la tabla",
+        "agudo chilla la tiza",
         "hoguera crepita leña",
-        "el transformador resuena"];
+        "el transformador rezumba"];
 
 function whatthehaiku(){
     hai = Math.floor(Math.random() * nat.length);
